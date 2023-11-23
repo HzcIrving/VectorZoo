6c008c8594efbcab0dc04ea3bcba890349cb566a
Irving <15357463558@163.com>
2023-11-23 19:50:20 +0800

    ArgoverseRos可视化

---
+++ Dataset/RosVisWkspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/src
+# Build directory: /mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ Dataset/RosVisWkspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/devel:$CMAKE_PREFIX_PATH"
+export PWD='/mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/mnt/ssd/ArgDataset/VectorZoo/Dataset/RosVisWkspace/src:$ROS_PACKAGE_PATH"
